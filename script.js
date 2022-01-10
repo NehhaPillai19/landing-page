2e6cf539063714308a87e8fc54b2257911117344
NehhaPillai19 <85875868+NehhaPillai19@users.noreply.github.com>
2022-01-10 22:44:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById('button1').addEventListener('click',
+function(){
+    document.querySelector('.p1').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.p1').style.display = 'none';
+})
+document.getElementById('button2').addEventListener('click',
+function(){
+    document.querySelector('.p2').style.display = 'flex';
+});
+document.querySelector('.close2').addEventListener('click',
+function(){
+    document.querySelector('.p2').style.display = 'none';
+})
+document.getElementById('button3').addEventListener('click',
+function(){
+    document.querySelector('.p3').style.display = 'flex';
+});
+document.querySelector('.close3').addEventListener('click',
+function(){
+    document.querySelector('.p3').style.display = 'none';
+})
